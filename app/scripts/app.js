79b0b3b127c01bda3b79d122b45acc07dd2f21e4
Jonnie Spratley <jonniespratley@gmail.com>
2013-10-19 11:10:01 -0700

    added pages and plugin

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,26 @@ angular.module('angularCmsBaseApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl'
+      })
+      .when('/docs', {
+        templateUrl: 'views/docs.html',
+        controller: 'DocsCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
+      })
+      .when('/settings', {
+        templateUrl: 'views/settings.html',
+        controller: 'SettingsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
