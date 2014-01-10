9ec6ff3492e0b6dc4ced2531695cbe011e8d6c9a
Jonnie Spratley <jonniespratley@me.com>
2014-01-09 18:28:39 -0800

    update

--- app/scripts/controllers/login.coffee
+++ app/scripts/controllers/login.coffee
@@ -1,5 +1,7 @@
 'use strict'
 angular.module('angularCmsApp').controller 'LoginCtrl', ($scope, $rootScope, $cookieStore) ->
+
+		$scope.name = 'login';
 		
 		#Setup initial model
 		$scope.user = null
--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
