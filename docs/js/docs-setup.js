3a44d771c97bcceb7310f2066c7ba25476ac439b
Jonnie Spratley <jonniespratley@gmail.com>
2014-03-18 20:52:46 -0700

    updated docs

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,35 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation",
+    "tutorial": "Angular CMS"
+  },
+  "pages": [
+    {
+      "section": "tutorial",
+      "id": "index",
+      "shortName": "0 - AngularCMS",
+      "type": "overview",
+      "moduleName": "0 - AngularCMS",
+      "shortDescription": "Angular CMS",
+      "keywords": "$10 $scope _activation _actual_ _id _key access accessing account activate active actual add adding address aditional adjust admin admin1234 administor agrees alert algorithm allow analytics angular angular-cms angularcms api app appear application assuming authenticate authentication avatar b7aa9e253f709fb3710fe05300d3056186ce92f7 backend base bootstrap bower branch broken browser browsers build built burger button calls catalog cdn ch3 change changes channels check checkbox choose clearfiles clearuploads cli click clicks clients clone cms code collection command commands configurations configure connected considered constructed content content-type contributing conventions create created creating credentials css current daily dashboard data database datagrid default defaults delete deletefile deleteupload dependencies describes design destroy development diagram diagrams dialog directed directly dispatches dist documentation don download drag drop dropboxusercontent dynamic e2e edit email employee employees enable enabled endpoint endpoints ensure enter enters epic events example examples execute exisiting explain fail failing farmers features file files find findall findbyid follow footer forgot fork form formats front-end functionality generated geo github global groups grunt grunt-cli gui guide hashed header help hired holds host html http implementation included incoming indentation input install installed installing instance instructions io issue isuploading joe js json keywords latest lightweight link listen ll local localhost locations login making manage management manager market master match matches media members message metadata method mimetype mock-up mock-ups modified modifying modules mongo mongodb multiple my-plugin my-theme node notes notified npm occur open opens orders org overview package partials pass password passwords path pending phantomjs place plugin plugins png post presented process product products project public pull push quick ready real receives records red redirected reflect register registration removes repeat repo request requests required reset resource restful rights role route routes running sale sales salt save scenario searching select sell sells server settings sh sha1 shouldn sidebar sign signed size socket sold source src start started starts stating stats status stay steps story storyboard structure stuff style subdirectory submit submitting substitute successful sudo superadmin supports system tablets tag terms test testing tests textual theme themes thing time title tom toms tools true tutorial twitter type ui unit update upload uploaded url user users valid verifies verify version view visual visualizing web website whitespace widgets work"
+    },
+    {
+      "section": "tutorial",
+      "id": "angularcms",
+      "shortName": "1 - AngularCMS",
+      "type": "overview",
+      "moduleName": "1 - AngularCMS",
+      "shortDescription": "Angular CMS",
+      "keywords": "angular angularcms bootstrap built cms js lightweight node overview tutorial twitter"
+    }
+  ],
+  "apis": {
+    "api": true,
+    "tutorial": false
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
