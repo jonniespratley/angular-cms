e161e46af09c9872fe3c6f788dde16812cd28d29
Jonnie Spratley <jonniespratley@me.com>
2014-03-26 18:14:19 -0700

    updated rest
    Former-commit-id: d3bf3765e9706b6d87cd7baa43d3c1b659e7cc8c

--- web.js
+++ web.js
@@ -12,5 +12,6 @@ app.get('/', function(req, res) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
+
