79305cf9a6f0664ae05165a18cec3b78e5ed86a3
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-15 16:21:28 -0700

    messing with proxy server

--- web.js
+++ web.js
@@ -1,14 +1,19 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var httpProxy = require('http-proxy');
 var app = express();
+var port = process.env.PORT || 5000;
+
 
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/dist'));
 app.use('/', express.directory('/dist'));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
+var http = require('http'),
+  httpProxy = require('http-proxy');
 
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(port);
