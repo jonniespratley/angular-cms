d38cf9a48804f637e92fab904313acc05177f12d
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-20 14:54:00 -0700

    added coveragalls support

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,57 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
 	config.set({
 		// base path, that will be used to resolve files and exclude
-		basePath : '.',
+		basePath: '.',
 
 		// testing framework to use (jasmine/mocha/qunit/...)
-		frameworks : ['jasmine'],
+		frameworks: ['jasmine'],
 
 		// list of files / patterns to load in the browser
-		files : ['app/bower_components/jquery/jquery.js',
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-animate/angular-animate.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-ui-utils/ui-utils.js',
-      'app/bower_components/angular-strap/dist/angular-strap.min.js',
-      'app/scripts/libs/parse-1.2.17.min.js', 'app/scripts/libs/md5.js',
-      'app/scripts/libs/markdown.js', '.tmp/scripts/*.js', '.tmp/scripts/**/*.js',
-		//'.tmp/mock/**/*.js',
-		'.tmp/spec/**/*.js'],
+		files: [
+
+			//Components
+			'app/bower_components/jquery/jquery.js',
+			'app/bower_components/angular/angular.js',
+			'app/bower_components/angular-animate/angular-animate.js',
+			'app/bower_components/angular-mocks/angular-mocks.js',
+			'app/bower_components/angular-resource/angular-resource.js',
+			'app/bower_components/angular-cookies/angular-cookies.js',
+			'app/bower_components/angular-sanitize/angular-sanitize.js',
+			'app/bower_components/angular-route/angular-route.js',
+			'app/bower_components/angular-ui-utils/ui-utils.js',
+			'app/bower_components/angular-strap/dist/angular-strap.min.js',
+
+			//Libs
+			'app/scripts/libs/parse-1.2.17.min.js',
+			'app/scripts/libs/md5.js',
+			'app/scripts/libs/markdown.js',
+
+			//Coffee source
+			//'app/scripts/**/*.coffee',
+			//'test/spec/**/*.coffee'
+
+			//Compile source
+			'.tmp/scripts/**/*.js',
+			'.tmp/spec/**/*.js'
+		],
 
 		// list of files / patterns to exclude
-		exclude : [],
+		exclude: [],
 
 		// web server port
-		port : 9090,
+		port: 9090,
 
 		// level of logging
 		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-		logLevel : config.LOG_INFO,
+		logLevel: config.LOG_DEBUG,
 
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch : false,
-
+		autoWatch: false,
+		// coverage reporter generates the coverage
+		reporters: ['progress', 'coverage'],
 		// Start these browsers, currently available:
 		// - Chrome
 		// - ChromeCanary
@@ -46,20 +60,24 @@ module.exports = function(config) {
 		// - Safari (only Mac)
 		// - PhantomJS
 		// - IE (only Windows)
-		browsers : ['PhantomJS'],
-		preprocessors : {
-			'.tmp/scripts/**/*.js' : ['coverage']
+		browsers: ['PhantomJS'],
+		preprocessors: {
+			//'app/scripts/**/*.coffee': ['coverage'],
+			//'test/spec/**/*.coffee': ['coffee']
+			'.tmp/scripts/**/*.js': ['coverage']
 		},
-		// coverage reporter generates the coverage
-		reporters : ['progress', 'coverage'],
+
 
 		// optionally, configure the reporter
-		coverageReporter : {
-			type : 'html',
-			dir : 'coverage/'
+		coverageReporter: {
+			reporters: [
+				{type: 'html', dir: 'coverage/'},
+				{type: 'lcov', dir: 'coverage/'},
+				{type: 'text-summary', dir: 'coverage/'}
+			]
 		},
 		// Continuous Integration mode
 		// if true, it capture browsers, run tests and exit
-		singleRun : false
+		singleRun: false
 	});
 };
