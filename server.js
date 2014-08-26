b8d6a3508ae212d3903e910b9649cf53a8a3907b
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-26 13:56:02 -0700

    updated package and removed socket.io

--- server.js
+++ server.js
@@ -93,9 +93,10 @@ var config = {
 
 //Start the reset server
 var rest = require('./routes/rest').rest;
+/*
 var socket = require('./routes/socketserver').SocketServer;
     socket.init(rest.init(config));
-
+*/
 
 //Create proxy server and proxy requests
 proxyServer = httpProxy.createServer(options, function(req, res, proxy) {
