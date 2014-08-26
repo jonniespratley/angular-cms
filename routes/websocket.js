28151d15fe3b71f8ccb29332ef449a89ff65b831
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-26 09:20:53 -0700

    updated websocket server

--- routes/websocket.js
+++ routes/websocket.js
@@ -6,13 +6,15 @@
 var http = require('http'),
 	fs = require('fs'),
 	url = require('url'),
-	WebSocketServer = require('websocket').server;
+	WebSocketServer = require('websocket').server,
+	WebSocketRouter = require('websocket').router;
 
 /**
  * Create Server
  */
 var server = http.createServer(function (req, res) {
 	var urlParsed = url.parse(req.url, true, true);
+
 	fs.readFile(urlParsed.path.split('/')[1], function (err, data) {
 		if (err) {
 			res.statusCode = 404;
@@ -21,6 +23,7 @@ var server = http.createServer(function (req, res) {
 		res.statusCode = 200;
 		res.end(data);
 	});
+
 }).listen(5050, function () {
 	console.log('WebSocket on port 5050');
 });
@@ -29,6 +32,14 @@ var serverConfig = {
 	httpServer: server,
 	autoAcceptConnections: false
 };
+
+var SocketServer = {
+	instance: null,
+	getInstance: function () {
+
+	}
+};
+
 
 /**
  * Create Socket Server
@@ -45,24 +56,24 @@ wsserver.on('connect', function (connection) {
 	connection.send('yo');
 });
 
-/**
- * On Request
- */
-wsserver.on('request', function (req) {
-	console.log('request');
-	var connection = req.accept('echo-protocol', req.origin);
-	connection.on('message', function (message) {
-		if (message.type === 'utf8') {
-			console.log(message.utf8Data);
-		}
-		else if (message.type === 'binary') {
-			console.log(message.binaryData);
-		}
+var router = new WebSocketRouter();
+router.attachServer(wsserver);
+router.mount('*', 'echo-protocol', function (request) {
+	console.log('mounted to echo protocol');
+	var conn = request.accept(request.origin);
+	conn.on('message', function (message) {
+		console.log('routed message');
 	});
-	connection.on('close', function (reasonCode, description) {
-		console.log('connection closed', reasonCode, description);
+	conn.send('hey');
+});
+router.mount('*', 'update-protocol', function (request) {
+	console.log('mounted to update protocol');
+	var conn = request.accept(request.origin);
+	conn.on('message', function (message) {
+		console.log('update all the things');
 	});
 });
+
 
 /**
  * On Close
