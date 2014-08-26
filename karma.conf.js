1949a361d67b3c855592ed3c116781f0b7c7ad8c
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-26 13:38:27 -0700

    updated package

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
 
 		// level of logging
 		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-		logLevel : config.LOG_INFO,
+		logLevel : config.LOG_DEBUG,
 
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch : false,
