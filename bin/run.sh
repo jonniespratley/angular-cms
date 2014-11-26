787a2b2ebbcfd5f7cde000e479960fbf917b0b47
Jonnie Spratley <jonniespratley@gmail.com>
2014-11-26 14:59:20 -0800

    run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-#mongod --dbpath db --rest; 
+
 #start the mongo db and server
-#mongod --dbpath db --rest; 
+mongod --dbpath db --rest --jsonp;
 
 #start the api server
 node server;
 
-#start the rest server
-#mongodb-rest;
- 
 #start the web server
-#bbb server;
-yeoman server;
+grunt serve;
