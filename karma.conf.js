457a97bdc43b2f8de44354ca3b79ebe64b01299c
Jonnie Spratley <jonniespratley@gmail.com>
2014-12-08 17:35:50 -0800

    fixed unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -23,6 +23,7 @@ module.exports = function (config) {
 			'app/bower_components/angular-route/angular-route.js',
 			'app/bower_components/angular-ui-utils/ui-utils.js',
 			'app/bower_components/angular-strap/dist/angular-strap.min.js',
+			'app/bower_components/angular-*/dist/angular-*.js',
 
 			//Libs
 			'app/scripts/libs/parse-1.2.17.min.js',
