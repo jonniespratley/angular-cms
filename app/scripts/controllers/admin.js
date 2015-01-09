222de632fd1a85b96cd87c7534ff7a33592cbfce
Spratley, Jonnie <jonnie.spratley@ge.com>
2015-01-09 14:58:52 -0800

    fixed unit tests

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,19 +1,8 @@
 'use strict';
-angular.module('angularCmsApp').controller('AdminCtrl', function ($scope, $rootScope, socket) {
+angular.module('angularCmsApp').controller('AdminCtrl', function ($scope, $rootScope) {
 	$scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
 	$scope.log = {};
 	$scope.msg = {};
-
-	$scope.send = function(obj){
-		socket.emit('create', obj)
-	};
-
-
-	socket.on('cms:client:message', function(e, data){
-		$scope.log += data;
-		console.log('message back', data);
-	});
-
 
 	console.log('admin ready');
 
