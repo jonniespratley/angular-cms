cbea63dd79042a3399abdc0ab1e869d6a3247b07
Jonnie Spratley <jonniespratley@gmail.com>
2015-01-18 14:00:48 -0800

    updated stackato

--- server.js
+++ server.js
@@ -7,5 +7,17 @@ var fs = require('fs');
 var cmsRouter = require('./routes/cms-router.js');
 var app = express();
 var config = JSON.parse(fs.readFileSync('./config/config.json'));
+
+//Test if services
+if(process.env.VCAP_SERVICES){
+  var cloudServices = JSON.parse(process.env.VCAP_SERVICES);
+
+  console.warn('cloud services', cloudServices);
+  //var dbcreds = services['mongodb'][0].credentials;
+}
+var port = process.env.PORT || 1339;
+var host = process.env.VCAP_APP_HOST || "127.0.0.1";
+config.host = host;
+config.port = port;
 var server = new cmsRouter.mount(config, app);
 console.log(server);
